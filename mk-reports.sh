ddef01e5321f1f6b476d3a1a4d4a27f0ebeed46e
Greg Heartsfield <scsibug@imap.cc>
2021-01-24 14:11:15 -0600

    Add --no-rounding option back in - it can be necessary sometimes.

--- mk-reports.sh
+++ mk-reports.sh
@@ -15,20 +15,20 @@ fi
 # Net worth (assets - liabilities) totals each month
 echo "* Report: Net Worth (monthly)"
 ledger -f $DAT_FILE reg Assets Liabilities \
-  $DATE_FMT -X \$ $PRICES \
+  $DATE_FMT -X \$ $PRICES --no-rounding \
   --monthly --total-data --collapse --empty \
   > "$REPORTS_DIR/networth.monthly.csv"
 
 # Expenses
 echo "* Report: Expenses (monthly)"
 ledger -f $DAT_FILE reg Expenses \
-  $DATE_FMT -X \$ $PRICES \
+  $DATE_FMT -X \$ $PRICES --no-rounding \
   --monthly --amount-data --collapse --empty \
   > "$REPORTS_DIR/expenses.monthly.csv"
 
 # Invested Assets (totals, monthly)
 echo "* Report: Invested Assets (monthly)"
 ledger -f $DAT_FILE reg Assets:Brokerage \
-  $DATE_FMT -X \$ $PRICES \
+  $DATE_FMT -X \$ $PRICES --no-rounding \
   --monthly --total-data --collapse --empty \
   > "$REPORTS_DIR/invested.monthly.csv"
